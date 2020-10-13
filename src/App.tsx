5f95e80ed9c8e138a080ce3d2c6ea1ca1c4d9316
Notreveu <66270887+Notreveu@users.noreply.github.com>
2020-10-12 22:23:21 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FiArrowRight} from 'react-icons/fi';
+
+import './styles/global.css';
+
+import Routes from './routes';
+
+
+
+
+function App() {
+  return (
+    <Routes/>
+    
+  );
+}
+
+export default App;
